c7e8d924763dbf7e81ef22f5113db144dfa5e46c
Juan Cardona <juanscr24@hotmail.com>
2023-09-07 10:56:49 -0500

    sider:complete

--- src/components/organisms/Sidebar/index.tsx
+++ src/components/organisms/Sidebar/index.tsx
@@ -1,14 +1,27 @@
 import { FC } from "react";
 import { sidebarItems } from "@/shared/constants/sidebar";
 import SidebarItem from "@/components/molecules/SidebarItem";
+import Link from "next/link";
+import Image from "next/image";
 
 const Sidebar: FC = () => {
   return (
-    <nav className="flex flex-col gap-1">
-      {sidebarItems.map((item) => (
-        <SidebarItem key={item.name} {...item} />
-      ))}
-    </nav>
+    <div>
+      <nav className="flex flex-col gap-1">
+        <Link href="/">
+          <Image
+            className="mt-1 flex items-center rounded-full gap-x-2 px-3 py-1 cursor-pointer w-fit hover:bg-slate-400 `"
+            width={20}
+            height={20}
+            src="/logo.png"
+            alt="logo"
+          />
+        </Link>
+        {sidebarItems.map((item) => (
+          <SidebarItem key={item.name} {...item} />
+        ))}
+      </nav>
+    </div>
   );
 };
 
